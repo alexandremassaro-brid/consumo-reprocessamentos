dcf7fa3c5e43e5d9b8136da35a5274ee73557c04
Alexandre Massaro <alexandre.massaro@bridsolucoes.com.br>
2024-10-14 11:10:33 -0300

    Mensagem significativa que demonstre as alterações
    Opcionalmente digite um descrição mais completa das alterações.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # consumo-reprocessamentos
+
+## Descrição
--- src/main.py
+++ src/main.py
@@ -11,6 +11,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 def main():
+  
   print('Main Function')
   username = os.getenv('USERNAME')
   password = os.getenv('PASSWORD')
